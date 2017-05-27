91c86f29ced8e4dbf4dd0d0c7eb5968e0482e3f1
Andrew Salcedo <asalcedo07@gmail.com>
2017-05-26 20:56:03 -0400

    add: add book form template. todo: submit btn listener

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,28 @@
     </div>
     <div class="panel panel-default">
       <div class="panel-heading">
+        <h3>Add Book</h3>
+      </div>
+      <div class="panel-body">
+        <form id="form" class="form-inline">
+          <div class="form-group">
+            <label for="bookTitle">Title</label>
+            <input type="text" id="bookTitle" class="form-control" v-model="newBook.title">
+          </div>
+          <div class="form-group">
+            <label for="bookAuthor">Author</label>
+            <input type="text" id="bookAuthor" class="form-control" v-model="newBook.author">
+          </div>
+          <div class="form-group">
+            <label for="bookUrl">Url</label>
+            <input type="text" id="bookUrl" class="form-control" v-model="newBook.url">
+          </div>
+          <input type="submit" class="btn btn-primary" value="Add Book">
+        </form>
+      </div>
+    </div>
+    <div class="panel panel-default">
+      <div class="panel-heading">
         <h3>Books Lists</h3>
       </div>
       <div class="panel-body">
@@ -56,6 +78,15 @@ export default {
   name: 'app',
   firebase: {
     books: booksRef
+  },
+  data() {
+    return {
+      newBook: {
+        title: '',
+        author: '',
+        url: ''
+      }
+    }
   }
 }
 </script>
