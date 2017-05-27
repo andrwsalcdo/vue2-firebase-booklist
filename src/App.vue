e0fe244060f064aa907f692643e158c09d3531e7
Andrew Salcedo <asalcedo07@gmail.com>
2017-05-26 20:45:38 -0400

    add app.vue template.todo: input new book

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,41 @@
 <template>
-  <div id="app">
-
+  <div id="app" class="container">
+    <div class="page-header">
+      <h1>Vue.js 2 & Firebase Sample Book App</h1>
+    </div>
+    <div class="panel panel-default">
+      <div class="panel-heading">
+        <h3>Books Lists</h3>
+      </div>
+      <div class="panel-body">
+        <table class="table table-striped">
+          <thead>
+            <tr>
+              <th>
+                Title
+              </th>
+              <th>
+                Author
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="book in books">
+              <td>
+                <a v-bind:href="book.url">{{ book. title }}</a>
+              </td>
+              <td>
+                {{ book. author }}
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import Hello from './components/Hello'
 
 import Firebase from 'firebase'
 
@@ -35,7 +65,6 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
 }
