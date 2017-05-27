63ee70c1462285a9334c30a1c8267d0cf96dcabc
Andrew Salcedo <asalcedo07@gmail.com>
2017-05-26 20:23:30 -0400

    first: firebase & bootstrap setup

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+
+  </div>
+</template>
+
+<script>
+import Hello from './components/Hello'
+
+import Firebase from 'firebase'
+
+let config = {
+  apiKey: "AIzaSyDQuyPKOe6U7zTtey2pTaucEad3DYg03ZI",
+  authDomain: "vue2-firebase-booklist.firebaseapp.com",
+  databaseURL: "https://vue2-firebase-booklist.firebaseio.com",
+  storageBucket: "vue2-firebase-booklist.appspot.com",
+  messagingSenderId: "212113090399"
+}
+
+let app = Firebase.initializeApp(config);
+let db = app.database();
+
+let booksRef = db.ref('books');
+
+export default {
+  name: 'app',
+  firebase: {
+    books: booksRef
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
